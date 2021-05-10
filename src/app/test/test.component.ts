5a178e102d009f620865af960061dd44e0841cb9
alisson.b <alissongilson.bezerra@gmail.com>
2021-05-10 13:08:58 -0300

    commit

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.scss'],
+})
+export class TestComponent implements OnInit {
+  gradient: string = 'linear-gradient(0deg, #000000, #ffffff)';
+
+  constructor() {}
+  ngOnInit(): void {}
+  changeColor(angle: string, color1: string, color2: string) {
+    this.gradient = `linear-gradient(${angle}deg, ${color1}, ${color2})`;
+  }
+}
