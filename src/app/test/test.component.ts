842d7dab14d63c3d16aa455fb08774abfd3e24d4
alisson.b <alissongilson.bezerra@gmail.com>
2021-05-12 07:55:59 -0300

    feat: adds follower

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -9,7 +9,15 @@ export class TestComponent implements OnInit {
   gradient: string = 'linear-gradient(0deg, #000000, #ffffff)';
 
   constructor() {}
-  ngOnInit(): void {}
+  
+  ngOnInit(): void {
+    let follower = document.querySelector<any>('#follower');
+
+    document.addEventListener('mousemove', function(e){
+      follower.style.left = e.pageX+'px';
+      follower.style.top = e.pageY+'px';
+    })
+  }
   changeColor(angle: string, color1: string, color2: string) {
     this.gradient = `linear-gradient(${angle}deg, ${color1}, ${color2})`;
   }
