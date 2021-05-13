b9994b797a625b33e7247df04e7849a1a9b7c184
alisson.b <alissongilson.bezerra@gmail.com>
2021-05-13 12:49:31 -0300

    feat: new follower style

--- src/app/follower/follower.component.ts
+++ src/app/follower/follower.component.ts
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./follower.component.scss'],
 })
 export class FollowerComponent implements OnInit {
-  nFollowers: number = 20;
+  nFollowers: number = 40;
   arrFollowers: Array<number> = [];
   counter: number = 0;
+  firstTime: boolean = true;
 
   constructor() {}
 
@@ -20,19 +21,47 @@ export class FollowerComponent implements OnInit {
     }
     window.addEventListener('load', function () {
       let followers = document.querySelectorAll<any>('.follower');
-      let multiplier = 0;
+      let multiplier = 0.5;
+      let randomMoving = false;
+      let mouseMoving = false;
+      let mousePosition: MouseEvent;
 
       followers.forEach((follower) => {
         follower.style.transition = `all ${multiplier}s`;
-        follower.style.transform = `translate(-50%, -50%) scale(${multiplier*4})`;
-        multiplier=multiplier+0.02;
+        follower.style.transform = `translate(-50%, -50%) `;
+        multiplier = multiplier + 0.03;
       });
+
       document.addEventListener('mousemove', function (e) {
-        followers.forEach((follower) => {
-          follower.style.left = e.pageX + 'px';
-          follower.style.top = e.pageY + 'px';
-        });
+        mouseMoving = true;
+        mousePosition = e;
+
+        if (!randomMoving) {
+          randomMoving = true;
+          setTimeout(() => {
+            randomMoving = false;
+          }, 100);
+
+          followers.forEach((follower) => {
+            let randomPosX =
+              Math.floor(Math.random() * 200 - 100) + e.pageX + 'px';
+            let randomPosY =
+              Math.floor(Math.random() * 200 - 100) + e.pageY + 'px';
+            follower.style.left = randomPosX;
+            follower.style.top = randomPosY;
+          });
+        }
+        mouseMoving = false;
       });
+
+      document.addEventListener('mousemove', ()=>{
+        followers.forEach((follower) => {
+          setTimeout(() => {
+            follower.style.left = mousePosition.pageX + 'px';
+            follower.style.top = mousePosition.pageY + 'px';
+          }, multiplier * 1000);
+        });
+      })
     });
   }
 }
